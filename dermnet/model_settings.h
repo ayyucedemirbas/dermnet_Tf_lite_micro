5c104d1f699fb5a82fbcdab509b370b305cbb9f7
Ayyuce Demirbas <a.ayyuced@gmail.com>
2022-07-16 16:54:17 +0300

    Update model_settings.h

--- dermnet/model_settings.h
+++ dermnet/model_settings.h
@@ -23,11 +23,11 @@ limitations under the License.
 // if you change your model you'll need to update these constants.
 constexpr int kNumCols = 96;
 constexpr int kNumRows = 96;
-constexpr int kNumChannels = 1;
+constexpr int kNumChannels = 3;
 
 constexpr int kMaxImageSize = kNumCols * kNumRows * kNumChannels;
 
-constexpr int kCategoryCount = 2;
+constexpr int kCategoryCount = 4;
 constexpr int kPersonIndex = 1;
 constexpr int kNotAPersonIndex = 0;
 extern const char* kCategoryLabels[kCategoryCount];
